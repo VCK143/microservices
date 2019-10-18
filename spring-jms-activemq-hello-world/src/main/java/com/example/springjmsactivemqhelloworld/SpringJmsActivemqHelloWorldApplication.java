5513434451b5bca3dbe2cd97274c2cda28621005
Prashanth <Prashanth@USM-2F-25>
2019-10-18 19:12:22 +0530

    New

---
+++ spring-jms-activemq-hello-world/src/main/java/com/example/springjmsactivemqhelloworld/SpringJmsActivemqHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjmsactivemqhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJmsActivemqHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJmsActivemqHelloWorldApplication.class, args);
+	}
+
+}
