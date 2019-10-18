6d67e9202ea656f5c01ae55fdd842eea9ce5fcf9
Prashanth <Prashanth@USM-2F-25>
2019-10-18 19:20:40 +0530

    eureka

---
+++ employee-eureka-server/src/main/java/com/example/employeeeurekaserver/EmployeeEurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.example.employeeeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableAdminServer
+public class EmployeeEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeEurekaServerApplication.class, args);
+	}
+
+}
